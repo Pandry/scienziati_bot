fcf9885dc5ed81e5598d0072f3acf3b048b6f67a
Andrea <andry25.9.99@gmail.com>
2018-11-03 22:27:31 +0100

    Versin bump

--- scienzati_bot.py
+++ scienzati_bot.py
@@ -110,7 +110,7 @@ Questo è il bot del gruppo @scienza e permette di usufruire di queste funzioni:
 	/revokelist
 	"""
 
-	version = "α0.1.2.6"
+	version = "α0.1.2.7"
 	
 	gdpr_message = "Raccogliamo il numero di messaggi, nickname, ID e ultima volta che l'utente ha scritto. Per richiedere l'eliminazione dei propri dati contattare un amministratore ed uscire dal gruppo"
 
